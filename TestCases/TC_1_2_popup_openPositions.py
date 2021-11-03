c4b6f86ee0fa46226d194d390370cbdc0cb862b0
Martin Drucks <laoche@web.de>
2021-11-03 21:59:39 +0100

    ...nein, TC deckt auch Wegklicken des Popups ab

--- TestCases/TC_1_2_popup_openPositions.py
+++ TestCases/TC_1_2_popup_openPositions.py
@@ -3,28 +3,41 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 def tc(driver): # -> bool
-	print("TC_1_2_popup_openPositions")
 	try:
 		openPositionsAlertBox = driver.find_element(By.ID, 'popmake-41440')
 		# openPositionsAlertBox = driver.find_element_by_id ( 'popmake-41440') # geht auch
 		# text = openPositionsAlertBox.text 
 	except NoSuchElementException  as nsex:
 		logging.info("TC_1_2_popup_openPositions: NoSuchElementException ")
-		print("TC_1_2_popup_openPositions: NoSuchElementException ")
 		return False
 	except Exception as ex:
-		print("EXC TC1_2: " + str(ex))
+		logging.error("EXC TC1_2: " + str(ex))
 		return False
 	# hier: Kasten für Offene Stellen existiert
 	text1 = openPositionsAlertBox.text
 	expectString = "Wir stellen ein!\nIT- und BackOffice-Mitarbeiter*innen gesucht!\nMehr Infos\nX"
-	print(expectString)
 	if text1 != expectString:
-		print(text1)
+		logging.info("TC_1_2_popup_openPositions: anderer Text:\n" + Text1)
 		return False 
-	return True
+	logging.info("TC_1_2_popup_openPositions PASSED Popup + Text ")
+	
+	X_Button = openPositionsAlertBox.find_element(By.CLASS_NAME, 'pum-close')
+	text2 = X_Button.text
+	print("text2 " + text2)
+	X_Button.click()
+	try: 
+		X_Button = openPositionsAlertBox.find_element(By.CLASS_NAME, 'pum-close')
+	except NoSuchElementException as nsex:
+		logging.info("TC_1_2_popup_openPositions PASSED X beendet Popup")
+		return True
+	logging.info("TC_1_2_popup_openPositions FAILED X beendet Popup NICHT")
+	return False
+
+	# Nur Klick auf X macht es weg, keine anderen Funktionen
+	# button.pum-close:nth-child(2)
 	
 	
+
 	# id= popmake-41440   Kasten für Offene Stellen
 	
 	# Schriftzug "Wir stellen ein":
