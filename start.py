7a3a8a9ec8bc65f40b2baac15ebf6b4d3e17626c
Martin Drucks <laoche@web.de>
2021-11-08 12:49:45 +0100

    8.11. start.py code aufgeräumt. Jetzt: Modulname als Variable, dann TC dynamisch importieren

--- start.py
+++ start.py
@@ -8,17 +8,9 @@ import logging
 from Utilities.readCfg import readConfig # as readConfig
 
 config = readConfig("./config.txt")
-# config.showAll()
 SeleniumRoot = config.get("SeleniumRoot")
 print(SeleniumRoot)
 
-# home pc
-# logging.basicConfig(filename= 'C:/Users/laoch/OneDrive/Dokumente/Meins/Eigenes_F/auticon/Python/SeleniumPython/Logs/log.txt', level=logging.INFO) 
-
-# work lab
-# logging.basicConfig(filename='C:/Users/Lap126/Documents/auticon/Lern/Testautomatisierung/SeleniumPython/Logs/log.txt', level=logging.INFO) 
-
-# mit Config
 logging.basicConfig(filename= SeleniumRoot + '/Logs/log.txt', level=logging.INFO) 
 	
 def start():
@@ -31,32 +23,22 @@ def main():
 	from selenium import webdriver
 	from selenium.webdriver.common.keys import Keys
 	
-	import time # import problem
-	# driverpath = "C:/Users/laoch/OneDrive/Dokumente/Meins/AndereProgramme_G/Work/Drivers/geckodriver.exe"
+	import time 
 	driverpath = config.get("gecko")
 	driver = webdriver.Firefox(executable_path=driverpath)
 		# https://stackoverflow.com/questions/49929374/notadirectoryerror-winerror-267-the-directory-name-is-invalid-error-while-inv
-	# from selenium.common.exceptions import [TheNameOfTheExceptionClass]
-	# sonst: erst schreiben, dann importe raussuchen
-	
-	## driver = None
-	driver.get("https://auticon.de") #  smoke test 
-	# print(driver.title)
-	# folder = Path('./TestCases')  # --- Unterverz für Python 
-	
-	## from TC_2_topline import tc
-	
+		 
+	driver.get("https://auticon.de") 
+
 	# from TestCases import TC_1_title as testcase 
 	# result = testcase.tc(driver)
 	# print(result)
-	# result in Log 
-	# 
 	
 	# from TestCases import TC_1_1_popup_cookies_deny as testcase # am worklab: f1
 	# result = testcase.tc(driver)
 	# print(result)
 	
-	from TestCases import TC_1_2_popup_openPositions as testcase # am worklab: f2
+	from TestCases import TC_1_2_popup_openPositions as testcase # home: f2 -- Merged
 	result = testcase.tc(driver)
 	print(result)
 	
@@ -68,7 +50,6 @@ def main():
 	# result = testcase.tc(driver)
 	# print(result)
 	
-	# zuletzt
 	driver.close()
 	driver.quit()
 	 
