8d6d47c5825dc0957c4242811ae2952cecc0453b
Martin Drucks <laoche@web.de>
2021-11-22 11:28:18 +0100

    jetzt: TC gibt einen String zurück, Name TC ist bekannt. Dann reporter umschreiben, dass der String zerlegt wird

--- TestCases/TC_1_2_popup_openPositions.py
+++ TestCases/TC_1_2_popup_openPositions.py
@@ -7,30 +7,26 @@ from selenium.webdriver.common.by import By
 import time 
 # Eigene Module  
 from Utilities import Reporter as reporter
-
+from reporter import report 
 
 def tc(driver): # -> bool
 
-	# reporter.report("TC1", str(True), "Laeuft")
 	TC = "TC_1_2_popup_openPositions" # TODO dynamisch - als Modulname
 	try:
 		openPositionsAlertBox = driver.find_element(By.ID, 'popmake-41440')
 		# openPositionsAlertBox = driver.find_element_by_id ( 'popmake-41440') # geht auch
 	except NoSuchElementException  as nsex:
-		logging.info("TC_1_2_popup_openPositions: NoSuchElementException ")
-		result = "Failed"
-		reason = "Popup kommt nicht "
-		reporter
-		return False ## ok
+		logging.info("TC_1_2_popup_openPositions: NoSuchElementException")
+		return "Popup kommt nicht" ## ok
 	except Exception as ex:
 		logging.error("EXC TC1_2: " + str(ex))
-		return False ## ok
+		return str(ex) ## ok
 	# hier: Kasten für Offene Stellen existiert
 	text1 = openPositionsAlertBox.text
 	expectString = "Wir stellen ein!\nIT- und BackOffice-Mitarbeiter*innen gesucht!\nMehr Infos\nX"
 	if text1 != expectString:
 		logging.info("TC_1_2_popup_openPositions: anderer Text:\n" + text1)
-		return False ## ok
+		return text1 ## ok
 	## logging.info("TC_1_2_popup_openPositions PASSED Popup + Text ")
 	
 	X_Button = openPositionsAlertBox.find_element(By.CLASS_NAME, 'pum-close')
@@ -45,8 +41,8 @@ def tc(driver): # -> bool
 		# logging.info(str(nsex))
 		if str(nsex) == 'Message: Element <button class="pum-close popmake-close" type="button"> could not be scrolled into view\n':
 			logging.info("TC_1_2_popup_openPositions PASSED X beendet Popup")
-			return True # ok 
-		logging.error("--andere Exception TC_1_2_popup_openPositions:" + str(nsex))
-		return False # ok
+			return "PASSED" # ok 
+		logging.error("ERROR  --andere Exception TC_1_2_popup_openPositions:" + str(nsex))
+		return "ERROR  --andere Exception TC_1_2_popup_openPositions:" + str(nsex) # ok
 	logging.info("TC_1_2_popup_openPositions FAILED X beendet Popup NICHT")
-	return False   # ok
+	return "FAILED"   # ok
