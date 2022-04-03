a6be959d200286077b78afbfb9d155c24402bb12
Martin Drucks <laoche@web.de>
2022-04-04 00:01:07 +0200

    in progress

--- TestCases/TC_2_topline.py
+++ TestCases/TC_2_topline.py
@@ -1,28 +1,50 @@
+
+
 import logging 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
+
+# TC ist abhängig davon, dass die Popups weg sind
+	# TC_1_1_popup_cookies_deny
+	# TC_1_2_popup_openPositions
+# -- TODO Blocked TC einführen?
+
+
 
 # title="Unsere Leistungen" 
 # menu-item-70
 # class="">Unsere Leistungen
 
 def tc(driver): # -> bool
-	print("TC_2_topline")
+	print("TC_2_topline - Mouse over")
+	elem_unsereLeistungenText = None
 	try:
-		elem = driver.find_elements(By.CLASS_NAME, 'Unsere Leistungen')		# no exc 
+		elem_unsereLeistungenText = driver.find_element(By.CSS_SELECTOR, '.vc_custom_1542025788808 > figure:nth-child(1) > a:nth-child(1) > img:nth-child(1)' ) 
+	except Exception as e:
+		print("EXC findelem mouse over: " + str(e))
+		return False
+	# .vc_custom_1542025788808 > figure:nth-child(1) > a:nth-child(1) > img:nth-child(1)
+	
+	try:
 		#object of ActionChains
 		actChainObj = ActionChains(driver)
-		subelem = driver.find_element(By.XPATH, '//text()="Warum auticon"') # sollte failen
-		actChainObj.move_to_element(elem).perform()    # mouse over 
+		actChainObj.move_to_element(elem_unsereLeistungenText).perform()    # mouse over 
+		# action = webdriver.ActionChains(driver)
+		# element = driver.find_element_by_id('your-id') # or your another selector here
+		# action.move_to_element(element)
+		# action.perform()
 		
-		# subelem = driver.find_elements(By.  Warum auticon
-		subelem = driver.find_element(By.XPATH, '//text()="Warum auticon"')
-		
+	
 	except Exception as ex:
 		print("EXC TC2: " + str(ex))
 		return False
 
 	return True
+	
+	# TC_2_topline
+# EXC TC2: Message: Given xpath expression "//text()="Warum auticon"" is invalid: TypeError: Document.evaluate: Result type mismatch
+
+# TC_2_topline: False
 
 	
--- start.py
+++ start.py
@@ -3,6 +3,8 @@
 # start für selenium python Test
 # evalTC(3, "len1", execFuncWithExc(lcs, ["abc", "daf"]) ,[1,"a"]) 
 # geht das in Selenium ?
+# Erste Quelle https://www.selenium.dev/selenium/docs/api/py/api.html
+# Zweite https://selenium-python.readthedocs.io/api.html
 
 import logging
 from Utilities.readCfg import readConfig # as readConfig
@@ -14,185 +16,38 @@ SeleniumRoot = config.get("SeleniumRoot")
 # print(SeleniumRoot)
 
 logging.basicConfig(filename= SeleniumRoot + '/Logs/log.txt', level=logging.INFO) 
+# print("kommt hier hin")
+	
 	
 def start(): # collects unexpected exceptions from main 
+	# print("Started")
 	try:
 		main()
 	except Exception as ex:
-		logging.error("EXC: " + str(ex))
+		logging.error("EXC main level: " + str(ex))
 
 def main():	
-	passed = 0
-	failed = 0
-	errors = 0
-	reporter.openReport()
-	print("Reporter.openReport()")
-	# reporter.report("TC1", str(True), "Laeuft")
-	# reporter.report("TCFailed", str(False), "Geht gar nicht")
-	passed = 44
-	failed = 22
-	errors = 11
-	# reporter.addStats(passed, failed, errors)
-	# reporter.closeReport()
-	# return 
-	
 	
 	from selenium import webdriver
 	from selenium.webdriver.common.keys import Keys
 	
-	import time 
+	# import time 
 	driverpath = config.get("gecko")
 	driver = webdriver.Firefox(executable_path=driverpath)
 		# https://stackoverflow.com/questions/49929374/notadirectoryerror-winerror-267-the-directory-name-is-invalid-error-while-inv
 		 
 	driver.get("https://auticon.de") 
-	## driver = None
+	from TestCases import TC_1_1_popup_cookies_deny as testcase # am worklab: f1
+	result = testcase.tc(driver)
+	print("TC_1_1_popup_cookies_deny: " + str(result))
 
-	# from TestCases import TC_1_title as testcase 
-	# result = testcase.tc(driver)
-	# print(result)
-
-	# driver.get("https://auticon.de") 
-	# 	 nochmal; nur direkt hiernach sind die Offenen Stellen drin
-	# TC_1_2_popup_openPositions
-	
-	# return # to just get the start page for further analysis or the elements
-	# from TestCases import TC_1_title as testcase 
-	# result = testcase.tc(driver)
-	# print(result)
-	# result in Log 
-	# 
-	
-	# from TestCases import TC_1_1_popup_cookies_deny as testcase # am worklab: f1
-	# result = testcase.tc(driver)
-	# print("TC_1_1_popup_cookies_deny: " + str(result))
+	from TestCases import TC_1_2_popup_openPositions as testcase # am worklab: f1
+	result = testcase.tc(driver)
+	print("TC_1_2_popup_openPositions: " + str(result))
 
 	
-	import importlib
-	module_name = "TestCases.TC_1_2_popup_openPositions"
-	module = importlib.import_module(module_name, package=None) 
-	result = module.tc(driver)
-	module_name = "root.dir.subdir." + module_name 
-	print(module_name)
-	tc_name_parts = module_name.split(".", -1)
-	tc_name = tc_name_parts[len(tc_name_parts) - 1]
-	# tc_name = lastpart(module_name, ".")
-	print(tc_name + ": " + result)
-	if result == "Passed":
-		passed = passed + 1
-		reporter.report(tc_name, result, "")
-	else:
-		failed = failed + 1
-		reporter.report(tc_name, "FAILED", result)
-	
-	
-	# from TestCases import TC_1_2_popup_openPositions as testcase # home: f2 -- Merged
-	# result = testcase.tc(driver)
-	# print(result)
+	from TestCases import TC_2_topline as testcase # am worklab: f1
+	result = testcase.tc(driver)
+	print("Done - TC_2_topline - Mouse over: " + str(result))
 
-	# from TestCases import TC_2_topline as testcase 
-	# result = testcase.tc(driver)
-	# print(result)
-	
-	# from TestCases import TC_4_menuLinksObenSort as testcase 
-	# result = testcase.tc(driver)
-	# print(result)
-	
-	driver.close()
-	driver.quit()
-	reporter.addStats(passed, failed, errors)
-	reporter.closeReport()
-	
-def execAllTestcases(): # alle TC in 
-	from selenium import webdriver
-	from selenium.webdriver.common.keys import Keys
-	
-	import time 
-	driverpath = config.get("gecko")
-	
-	driver = webdriver.Firefox(executable_path=driverpath)
-	
-	# https://stackoverflow.com/questions/49929374/notadirectoryerror-winerror-267-the-directory-name-is-invalid-error-while-inv	 
-	driver.get("https://auticon.de") 
-	passed = 0
-	failed = 0
-	errors = 0
-	import os
-	myroot = config.get("SeleniumRoot")
-	print ("myroot = " + myroot)
-			# path =r'C:\Users\laoch\OneDrive\Dokumente\Meins\Eigenes_F\auticon\Python\SeleniumPython\TestCases'
-			## print ("path = " + path)
-	path = myroot + r'\TestCases'
-	print ("path = " + path)
-	ignorePath = path + '\__pycache__'
-	# print("ignorePath: " + ignorePath)
-	
-	for root, directories, file in os.walk(path): # root = path 
-		print("\nroot: " + str(root))
-		## print("directories: " + str(directories))
-		## print("file: " + str(file))
-		for onefile in file: 
-			if root != ignorePath:
-				if(onefile.endswith(".py")):
-					## print("file= " + onefile)
-					module_name = onefile[0:len(onefile)-3]
-					module_name = root + "\\" + module_name    
-						# läuft in Windows TODO ... Linux, Unix: Pfad erst umfummeln
-					# print ("module_name = " + module_name) #  Pfad ohne .py
-				
-					module_name = module_name[len(myroot)+1: len(module_name)]
-					module_name = module_name.replace("\\", ".", 100)
-
-					print ("module_name = " + module_name) #  Pfad ohne .py
-				#C:\Users\laoch\OneDrive\Dokumente\Meins\Eigenes_F\auticon\Python\SeleniumPython\TestCases\TC_1_1_popup_cookies_deny#
-					
-					## String befummeln: DONE 2.12.
-					## myroot = C:\Users\laoch\OneDrive\Dokumente\Meins\Eigenes_F\auticon\Python\SeleniumPython
-					## No module named 'C:\\Users\\laoch\\OneDrive\\Dokumente\\Meins\\Eigenes_F\\auticon\\Python\\SeleniumPython\\TestCases\\TC_1_1_popup_cookies_deny'
-					## 1. StrAfterStr(str, startstr)     
-					## 2. StrAfterStr(module_name, myroot) --> TestCases\TC_1_1_popup_cookies_deny
-					## 3. replace \ -> .
-					
-					
-					# module_name = "TestCases.TC_1_1_popup_cookies_deny"    # so gehts
-					
-					
-					## von main()
-					# module_name = "TestCases.TC_1_2_popup_openPositions"
-					module = importlib.import_module(module_name, package=None) 
-					
-					
-					## return
-						# No module named 'C:\\Users\\laoch\\OneDrive\\Dokumente\\Meins\\Eigenes_F\\auticon\\Python\\SeleniumPython\\TestCases\\TC_1_1_popup_cookies_deny'
-					
-					result = module.tc(driver)
-					
-					# module_name = "root.dir.subdir." + module_name 
-					# print(module_name)
-					tc_name_parts = module_name.split(".", -1)
-					tc_name = tc_name_parts[len(tc_name_parts) - 1]
-					# tc_name = lastpart(module_name, ".")
-					print(tc_name + ": " + str(result))
-					if result == "Passed":
-						passed = passed + 1
-						reporter.report(tc_name, result, "")
-					else:
-						failed = failed + 1
-						reporter.report(tc_name, "FAILED", result)
-					
-					
-					# import modulename
-					# result =  # TC_1_2_popup_openPositions
-					# print(os.path.join(root,file))
-			# else: print("ignorePath")	
-	driver.close()
-	driver.quit()
-	reporter.addStats(passed, failed, errors)
-	reporter.closeReport()
-			
-execAllTestcases()	
-# start()
-
-# def execFuncWithExc(functionName, args): # -> result, Exception given as String from Exception. functionName not in "" 
-	# try:
-		# result = functionName(*args)
+start()
