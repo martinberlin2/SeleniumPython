e4152e6bc4891bbbe7a6c7cd9491a90f901fb200
Martin Drucks <laoche@web.de>
2022-05-23 00:43:26 +0200

    skript steht fast

--- ExecLayerScripts/DDT_script.py
+++ ExecLayerScripts/DDT_script.py
@@ -1,21 +1,9 @@
 
 
-Data Driven Test Exec Script
-ModuleName DIV Tinit Einit Pinit
-TestcaseName DIV init init ['Param1', 'Param2']
-TestcaseName DIV t1 6 ['30', '5']
-TestcaseName DIV t2 606 ['6666', '101']
-TestcaseName DIV t3 DivByZero ['34', '0']
-TestcaseName DIV t4 777 ['42', '6']
-ModuleName  CollatzStep  777 ['42', '6']
-TestcaseName  CollatzStep init init ['Param1']
-TestcaseName  CollatzStep Odd1 16 ['5']
-TestcaseName  CollatzStep Odd2 17 ['52']
-TestcaseName  CollatzStep Even1 16 ['8']
-TestcaseName  CollatzStep Even2 2 ['1']
-ModuleName  CollatzStep  2 ['1']
-TestcaseName  CollatzStep init init ['Param1']
-TestcaseName  CollatzStep Odd1 52 ['17']
-TestcaseName  CollatzStep Even1 26 ['52']
-TestcaseName  CollatzStep Even2 13 ['26']
-TestcaseName  CollatzStep Odd2 40 ['13']
+# Data Driven Test Exec Script
+
+import Testcases.CollatzStep as TC  
+exec(Testcases.CollatzStep.tc, [, Odd1, 16, 5])
+exec(Testcases.CollatzStep.tc, [, Odd2, 4, 1])
+exec(Testcases.CollatzStep.tc, [, Even1, 16, 8])
+exec(Testcases.CollatzStep.tc, [, Even2, 5, 10])
--- ExecLayerScripts/ReadCsvTestdataWithFuncNames.py
+++ ExecLayerScripts/ReadCsvTestdataWithFuncNames.py
@@ -1,105 +1,66 @@
 
 
-	# ReadAndWriteCSV
 	# https://docs.python.org/3/library/csv.html#reader-objects
-
-	# C:\Users\laoch\OneDrive\Dokumente\Meins\Eigenes_F\auticon\Python\SeleniumPython\TestData
-	# DD_Testdata.csv 
-import csv
-
+	
+import csv # finder reader nicht
 from Utilities.readCfg import readConfig 
 config = readConfig("./config.txt")
 SeleniumRoot = config.get("SeleniumRoot")
-
-# SeleniumRoot = r"D:\Travel\SeleniumPython"   # ReiseLab
-## print("SeleniumRoot = " + SeleniumRoot) 
-td_filename = SeleniumRoot + r"\TestData\DD_Testdata.csv"
-## print (str(td_filename))
+script_filename = SeleniumRoot + r"\ExecLayerScripts\DDT_script.py"
 
 import os.path
-script_filename = SeleniumRoot + r"\ExecLayerScripts\DDT_script.py"
 if os.path.isfile(script_filename) == True:
 	os.remove(script_filename)
-# with open(script_filename, "a") as dd_script:
-		# print("OutfileName:", outfile)
-		# dd_script.write(line)
+	
+# SeleniumRoot = r"D:\Travel\SeleniumPython"   # ReiseLab
+## print("SeleniumRoot = " + SeleniumRoot) 
+
+# ModuleName;Testcases.CollatzStep
+# TestcaseName;ExpectedResult;Param1;;
+# Odd1;16;5;;
+# Odd2;4;1
+# Even1;16;8;;
+# Even2;5;10;;
+
 
 def run():
-	LastLine = "Empty"
+# TODO Für alle .py as aktPy in Execution List: # daher td_filename
+	aktTC = SeleniumRoot + r"\TestCases\CollatzStep"
+	td_filename = SeleniumRoot + r"\TestCases\CollatzStep_Testdata.csv"
+	# if not exist, normal TC 
+	print (str(td_filename))
 	with open(td_filename, newline='') as td, open(script_filename, "a") as dd_script:
-		dd_script.write("\n\nData Driven Test Exec Script\n")
-		testdata = csv.reader(td, delimiter = ";")
-		TestcaseName = "Tinit"
-		ExpectedResult = "Einit"
-		params = "Pinit"
-		ModuleName = "Minit"
+		testdata = csv.reader (td, delimiter = ";")
+		if csv == None:
+			print("if csv == None:")
+		# existTestdata() ; nein -> cols = 0, ja -> cols = 1
+		ModuleName = "Testcases.CollatzStep"
+		dd_script.write("\n\n# Data Driven Test Exec Script\n")
+		dd_script.write("\nimport " + ModuleName + " as TC  \n")
+		# testdata = csv.reader(td, delimiter = ";")
+		# csv.reader(csvfile, dialect='excel', **fmtparams)
+		rows = 1
 		for row in testdata:
-			# print(LastLine + "#" + str(row))
-			if LastLine == "TestcaseName":
-				TestcaseName = row[0]
-				if TestcaseName == "":
-					erg = "TestcaseName empty" # TODO line, file...
-					# print (erg)
-					LastLine = "Empty"
-					continue 
-				else:	
-					ExpectedResult = row[1]  # darf leer sein
-					c = 2
-					params = []
-					while c <= cols: # row[c] != "":
-						print(str(row[c]))
-						params.append(row[c])
-						print("params: " + str(params))
-						c = c + 1
-					# cols = c - 1
-					# while c <= cols:
-						# print(row[c])
-						# c = c + 1
-						
-			if LastLine == "ModuleName":
-				TestcaseName = "init"
-				ExpectedResult = "init"
-				params = "init"
-				header = row[0]
-				if header != "TestcaseName": 
-					erg = "Error TestcaseName header" # TODO line, file...
-					print (erg)
-					return erg
-				header = row[1]
-				if header != "ExpectedResult": 
-					erg = "Error ExpectedResult header" # TODO line, file...
-					print (erg)
-					return erg
-				c = 2
-				params = []
-				while row[c] != "":
-					print(str(row[c]))
-					params.append(row[c])
-					print("params: " + str(params))
-					c = c + 1
-				cols = c - 1
-				print ("cols = " + str(cols))
-				# params = params + "]"
-				# print ("cols gesetzt: " + str(cols))
-				LastLine = "TestcaseName"
-			if LastLine == "Empty" :
-				header = row[0]
-				if header == "":
-					print ("--- Ende Testdata ---")
+			if rows > 2:
+				print(row[0] + "\n")
+				if row[0] == None: # "":
+					print("## Ende ## + row[0]")
 					break
-				if header != "ModuleName": 
-					erg = "Error ModuleName header" # TODO line, file...
-					print (erg)
-					return erg
-				ModuleName = row[1]
-				if header == "":
-					erg = "Error functionName header" # TODO line, file...
-					print (str(erg))
-					return erg
-				LastLine = "ModuleName"	
-			dd_script.write(LastLine + " " + ModuleName + " " + TestcaseName + " " + ExpectedResult + " " + str(params) + "\n")
-		print("---- Ende Testdata ----")
-		return 
-	# Ende with open(td_filename, newline='')
-
-	# ExpectedResult	Param1	Param2
+				out  = "exec(" + ModuleName + ".tc, ["
+				# wenn Testdaten: if existTestdata()
+				c = 0
+				while c < cols:
+					out = out + ", " + str(row[c])
+					c=c+1	
+				out = out + "])"
+				dd_script.write(out + "\n")
+			if rows == 2:
+				cols = 0
+				while row[cols] != "":
+					# print(row[cols] + "\n")
+					cols = cols + 1
+				# cols = cols - 1
+				rows = rows + 1
+			if rows == 1:
+				rows = rows + 1
+		# script_filename.close 
