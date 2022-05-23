8276a1f02325b24eb8f87afb66ce6075fc946025
martinberlin2 <70579601+martinberlin2@users.noreply.github.com>
2022-05-23 18:45:01 +0200

    Suche nach Locator, und was sagt Locator not found aus - muss ich scrollen oder findet er nix ? erstmal Scrollen probieren, Suchergebnisse gesammelt und Code vorbereitet. Scroll geht schon, Element finden noch nicht

--- TestCases/TC_5_Scrolling_Elem_Present_not_Visible.py
+++ TestCases/TC_5_Scrolling_Elem_Present_not_Visible.py
@@ -1,3 +1,5 @@
+
+
 # TC_5_Scrolling_Elem_Present_not_Visible
 # Desktop Modus, Vollbild und Teilbild 
 # TC ist abhängig davon, dass die Popups weg sind
@@ -8,39 +10,59 @@
 # Selenium / Python imports
 import logging 
 from selenium.common.exceptions import NoSuchElementException  
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 import time 
+
+# #text-2 > div:nth-child(1) > p:nth-child(1) > a:nth-child(1)
+# Link: AGB , unten auf Homepage
+# Link zu: https://auticon.de/allgemeine-geschaeftsbedingungen-agb/
+# ohne Scrollen: NoSuchElementException
 
 def tc(driver): # -> bool
 	TC = "TC_5_Scrolling_Elem_Present_not_Visible" # TODO dynamisch - als Modulname
 	print(TC + " start")
+	cssSelector = 'text-2 > div:nth-child(1) > p:nth-child(1) > a:nth-child(1)'
 	try:
-		openPositionsAlertBox = driver.find_element(By.ID, 'popmake-41440')
-	except NoSuchElementException  as nsex:
-		logging.info(TC + ": NoSuchElementException")
-		return TC + ": Popup kommt nicht"
+		AGB_link = driver.find_element(driver.find_element(By.CSS_SELECTOR, cssSelector))
+	except NoSuchElementException as nsex:
+		toReturn = TC + ": NoSuchElementException - jetzt Scroll"
+		logging.info(toReturn)
+		# return toReturn
 	except Exception as ex:
-		logging.error(TC + ": " + str(ex))
+		logging.error(TC + "Andere Exception: " + str(ex))
+		return str(ex) 
+	print("nach Exception noch drin")
+	
+	# https://stackoverflow.com/questions/20986631/how-can-i-scroll-a-web-page-using-selenium-webdriver-in-python
+	# da auch "unendlicher" Scroll 
+	# hover = ActionChains(driver).move_to_element(elem)
+			# hover.perform()
+
+	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+	# to scroll to the bottom of the page.
+
+	# ODER
+	# label.sendKeys(Keys.PAGE_DOWN);
+	
+	# ODER
+	# from selenium.webdriver.common.keys import Keys
+	# html = driver.find_element_by_tag_name('html')
+	# html.send_keys(Keys.END)
+	
+	try:
+		AGB_link = driver.find_element(driver.find_element(By.CSS_SELECTOR, cssSelector))
+	except NoSuchElementException as nsex:
+		toReturn = TC + ": NoSuchElementException nach Scroll"
+		logging.info(toReturn)
+		return toReturn
+	except Exception as ex:
+		logging.error(TC + "Andere Exception nach Scroll: " + str(ex))
 		return str(ex) 
 	
-	text1 = openPositionsAlertBox.text
-	expectString = "Wir stellen ein!\nIT- und BackOffice-Mitarbeiter*innen gesucht!\nMehr Infos\nX"
+	textLink = AGB_link.text
+	expectTextLink = "AGB"
 	if text1 != expectString:
 		logging.info(TC + ": anderer Text:\n" + text1)
-		return ("anderer Text:\n" + text1)
-	
-	X_Button = openPositionsAlertBox.find_element(By.CLASS_NAME, 'pum-close')
-	X_Button.click()     # Fenster incl. Button verschwindet jetzt --- WAIT !
-	time.sleep(1)  # braucht 1 sek, nicht 5 --- 18.11.
-	try: 
-		X_Button = openPositionsAlertBox.find_element(By.CLASS_NAME, 'pum-close')
-		X_Button.click()
-	except Exception as nsex:
-		# logging.info(str(nsex))
-		if str(nsex) == 'Message: Element <button class="pum-close popmake-close" type="button"> could not be scrolled into view\n':
-			logging.info(TC + ": PASSED X beendet Popup")
-			return "Passed"
-		logging.error("ERROR  --andere Exception " + TC + ":" + str(nsex))
-		return "ERROR  --andere Exception " + TC + ":" + str(nsex)
-	logging.info("" + TC + " FAILED X beendet Popup NICHT")
-	return "X beendet Popup NICHT"
+		return ("Scroll OK ! anderer Text:\n" + text1)
+	return "Passed"
